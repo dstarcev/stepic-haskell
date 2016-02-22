e108f779685ce848dabb8047841838414a49861c
dstarcev <starcev.da@gmail.com>
2016-02-23 00:27:03 +0300

    Add solution to task 5 of module 1

---
+++ task_1_5/Main.hs
@@ -0,0 +1,5 @@
+discount :: Double -> Double -> Double -> Double
+discount limit proc sum = if sum >= limit then sum * (100 - proc) / 100 else sum
+
+standardDiscount :: Double -> Double
+standardDiscount = discount 1000 5
