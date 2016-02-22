e4d10b9fe87c51e4089654f1b0be1aeff88f72a9
dstarcev <starcev.da@gmail.com>
2016-02-23 02:43:31 +0300

    Add solution to task 10 of module 1

---
+++ task_1_10/Main.hs
@@ -0,0 +1,10 @@
+fibonacci :: Integer -> Integer
+fibonacci n
+  | n == 0 = 0
+  | n == 1 = 1
+  | n < 0 = -(-1) ^ (-n) * fibonacci (-n)
+  | n > 0 = fibonacciIter 0 1 (n - 2) 
+
+fibonacciIter acc1 acc2 0 = acc1 + acc2
+fibonacciIter acc1 acc2 n =
+  fibonacciIter (acc2) (acc1 + acc2) (n - 1)
