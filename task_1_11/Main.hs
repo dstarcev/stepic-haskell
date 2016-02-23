cee60621af1c85b15ab7298473eb425783a8b104
dstarcev <starcev.da@gmail.com>
2016-02-23 03:16:43 +0300

    Add solution to task 11 of module 1

---
+++ task_1_11/Main.hs
@@ -0,0 +1,10 @@
+seqA :: Integer -> Integer
+seqA n
+  | n == 0 = 1
+  | n == 1 = 2
+  | n == 2 = 3
+  | otherwise = let
+      iter a0 a1 a2 (-1) = a2
+      iter a0 a1 a2 n =
+        iter a1 a2 (a2 + a1 - 2 * a0) (n - 1)
+    in iter 1 2 3 (n - 3)
