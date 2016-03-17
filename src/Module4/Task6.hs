6d8d81db1549e0d385403dd325c346ed21db2e8e
dstarcev <starcev.da@gmail.com>
2016-03-17 22:29:12 +0300

    Add solution to task 6 of module 4

---
+++ src/Module4/Task6.hs
@@ -0,0 +1,7 @@
+module Module4.Task6 where
+
+data Point = Point Double Double
+
+distance :: Point -> Point -> Double
+distance (Point x1 y1) (Point x2 y2) =
+  sqrt $ (x1 - x2) ^ 2 + (y1 - y2) ^ 2
