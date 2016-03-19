0a2396c572b517583b1ea011357e7dc7257b4b2d
dstarcev <starcev.da@gmail.com>
2016-03-19 13:24:49 +0300

    Add solution to task 18 of module 4

---
+++ src/Module4/Task18.hs
@@ -0,0 +1,9 @@
+module Module4.Task18 where
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just x) = [x]
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x:_) = Just x
